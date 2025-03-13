633d9101d5d61afcd30a1f51e64a5a91fe426f82
Miguel Angel Ramirez Lopez <C-MiguelAngel.Ramirez@charter.com>
2025-03-13 16:30:24 -0500

    review 001

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import routeConfig from './app/routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch } from "@angular/common/http";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { provideRouter } from "@angular/router";
+import { AppComponent } from "./app/app.component";
+import routeConfig from "./app/routes";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routeConfig), provideAnimationsAsync(), provideHttpClient(withFetch()),],
+  providers: [
+    provideRouter(routeConfig),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+  ],
 }).catch((err) => console.error(err));
